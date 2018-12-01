c9c4c7130b9fcee9b7b3c0e9464987446c509010
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-12-01 18:56:27 +0545

    using es6 import export

--- src/index.js
+++ src/index.js
@@ -1,1 +1,6 @@
+import addnumber, { subtract } from './math';
+
 console.log('Hello Graphql')
+
+console.log(addnumber(2, 4))
+console.log(subtract(2, 4))
---
+++ src/math.js
@@ -0,0 +1,5 @@
+const add = (num1, num2) => num1 + num2;
+
+const subtract = (num1, num2) => num1 - num2;
+
+export { add as default, subtract };
