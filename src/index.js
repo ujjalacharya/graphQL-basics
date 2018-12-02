fca2a26935b7fe2558d71d0161fc2f1ca1fca128
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-12-02 11:48:50 +0545

    add query method that takes arguement

--- src/index.js
+++ src/index.js
@@ -1,46 +1,64 @@
 import { GraphQLServer } from "graphql-yoga";
 
-/*
-title String
-price foat
-realeaseYear int-opt
-rating  float-opt
-inStock Boolean
-*/
-
 const typeDefs = `
   type Query{
-    title: String!
-    price: Float!
-    releaseYear: Int
-    rating: Float
-    inStock: Boolean!
+    greetings(name: String): String!
+    me: User!
+    post: Post!
+    add(num1: Float!, num2: Float!): Float!
   }
-`
 
+  type User{
+    id: ID!,
+    name: String!,
+    email: String,
+    age: Int
+  }
+
+  type Post{
+    id: ID!,
+    title: String!,
+    body: String!,
+    isPublished: Boolean!
+  }
+
+`;
 const resolvers = {
-  Query:{
-    title(){
-      return 'Nike Shoes'
+  Query: {
+    add(parent, args){
+      let {num1, num2} = args;
+      return num1+num2
     },
-    price(){
-      return 2400.5
+    greetings(parent, args){
+      if(args.name){
+        return `Hey there, ${args.name}!`
+      }
+      return 'Hey there, Guest!'
     },
-    releaseYear(){
-      return 2015
+    me() {
+      return {
+        id: "12345",
+        name: "Ujjal",
+        email: "acharyaujjal1@gmail.com",
+        age: 22
+      };
     },
-    rating(){
-      return 2.5
-    },
-    inStock(){
-      return true
-    },
-
+    post(){
+      return{
+        id: 'abc12',
+        title: 'Post about dogs',
+        body: 'Some description on dogs',
+        isPublished: true
+      }
+    }
   }
-}
+};
 
-const server = new GraphQLServer({typeDefs, resolvers})
+const server = new GraphQLServer({
+  typeDefs,
+  resolvers
+});
 
 server.start(() => {
-  console.log("GraphQL server is up and running!");
+  console.log("Graphql server is up and running at port 4000");
 });
