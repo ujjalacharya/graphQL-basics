23c189bf4de42a5c057b4cc843e8186e86df8609
AcharyaUjjal1 <acharya_ujjal@yahoo.com>
2018-12-02 09:50:34 +0545

    add additional Query type

--- src/index.js
+++ src/index.js
@@ -1,23 +1,45 @@
 import { GraphQLServer } from "graphql-yoga";
 
+/*
+title String
+price foat
+realeaseYear int-opt
+rating  float-opt
+inStock Boolean
+*/
+
 const typeDefs = `
   type Query{
-    hello: String!
+    title: String!
+    price: Float!
+    releaseYear: Int
+    rating: Float
+    inStock: Boolean!
   }
-`;
+`
 
 const resolvers = {
-  Query: {
-    hello() {
-      return "This is my first Query";
-    }
-  }
-};
+  Query:{
+    title(){
+      return 'Nike Shoes'
+    },
+    price(){
+      return 2400.5
+    },
+    releaseYear(){
+      return 2015
+    },
+    rating(){
+      return 2.5
+    },
+    inStock(){
+      return true
+    },
 
-const server = new GraphQLServer({
-  typeDefs,
-  resolvers
-});
+  }
+}
+
+const server = new GraphQLServer({typeDefs, resolvers})
 
 server.start(() => {
   console.log("GraphQL server is up and running!");
